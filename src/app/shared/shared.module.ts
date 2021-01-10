6bd476459acef46dd3cde267d630ac6669500ab1
jakubdrewniak <drewniak.jakub@gmail.com>
2021-01-10 13:15:53 +0100

    translations in lazy loaded modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './components/shared/shared.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
   declarations: [SharedComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    TranslateModule
   ],
-  exports: [SharedComponent]
+  exports: [SharedComponent, TranslateModule],
 })
-export class SharedModule { }
+export class SharedModule {}
